aa707f58ae8954b8adacd93d45fe74748eca0831
Harrison Yu <harrisonyu6@gmail.com>
2013-11-25 14:47:58 -0600

    added velocity

--- src/com/example/easyauthentication/MainActivity.java
+++ src/com/example/easyauthentication/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.easyauthentication;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.view.VelocityTrackerCompat;
+import android.util.Log;
+import android.view.*;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private static final String DEBUG_TAG = "Velocity";
 
+	private TextView velocityDisplay;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        velocityDisplay = (TextView)findViewById(R.id.velocity);
         setContentView(R.layout.activity_main);
     }
 
@@ -19,5 +26,50 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    private VelocityTracker mVelocityTracker = null;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
+
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+                if(mVelocityTracker == null) {
+                    // Retrieve a new VelocityTracker object to watch the velocity of a motion.
+                    mVelocityTracker = VelocityTracker.obtain();
+                }
+                else {
+                    // Reset the velocity tracker back to its initial state.
+                    mVelocityTracker.clear();
+                }
+                // Add a user's movement to the tracker.
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker.addMovement(event);
+                // When you want to determine the velocity, call 
+                // computeCurrentVelocity(). Then call getXVelocity() 
+                // and getYVelocity() to retrieve the velocity for each pointer ID. 
+                mVelocityTracker.computeCurrentVelocity(1000);
+                // Log velocity of pixels per second
+                // Best practice to use VelocityTrackerCompat where possible.
+                Log.d("", "X velocity: " + 
+                        VelocityTrackerCompat.getXVelocity(mVelocityTracker, 
+                        pointerId));
+                Log.d("", "Y velocity: " + 
+                        VelocityTrackerCompat.getYVelocity(mVelocityTracker,
+                        pointerId));
+        		/*velocityDisplay.setText("X velocity: " + VelocityTrackerCompat.getXVelocity(mVelocityTracker, 
+                        pointerId) + " Y velocity: " + VelocityTrackerCompat.getYVelocity(mVelocityTracker,
+                                pointerId));*/
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // Return a VelocityTracker object back to be re-used by others.
+                mVelocityTracker.recycle();
+                break;
+        }
+        return true;
+    }
 }
